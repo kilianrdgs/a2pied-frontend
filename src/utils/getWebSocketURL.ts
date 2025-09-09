1128c9c966e61369a09574d7fc71f5a231ce74e9
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-09 09:47:00 +0200

    quickfix retrait consolelog

--- src/utils/getWebSocketURL.ts
+++ src/utils/getWebSocketURL.ts
@@ -1,9 +1,6 @@
 export const getWebSocketURL = () => {
     const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL;
 
-    console.log("WEBSOCKET_URL")
-    console.log(WEBSOCKET_URL)
-
     if (!WEBSOCKET_URL) throw new Error()
     return WEBSOCKET_URL
 }
