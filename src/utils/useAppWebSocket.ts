23cf1cab07d46a35db2fc734adf3ced77e74e3a6
Dylano2 <dylan.gonzalez@outlook.fr>
2025-09-12 15:54:22 +0200

    quickfix import

--- src/utils/useAppWebSocket.ts
+++ src/utils/useAppWebSocket.ts
@@ -1,19 +1,17 @@
 // useAppWebSocket.ts
-import { useEffect, useMemo } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
-import { getWebSocketURL } from "./getWebSocketURL.ts";
-import {
-	type WebsocketCommunicationC2SType,
-	WebsocketEventC2SEnum,
-} from "./WebsocketCommunicationC2SType.ts";
+import {useEffect, useMemo} from "react";
+import useWebSocket, {ReadyState} from "react-use-websocket";
+import {getWebSocketURL} from "./getWebSocketURL.ts";
+import {type WebsocketCommunicationC2SType, WebsocketEventC2SEnum,} from "./WebsocketCommunicationC2SType.ts";
+import type {WebsocketCommunicationS2CType} from "./WebsocketCommunicationS2CType.ts";
 
 type UseAppWebSocketOptions = {
-	autoSyn?: boolean;
-	email: string;
+    autoSyn?: boolean;
+    email: string;
 };
 
 export function useAppWebSocket(options: UseAppWebSocketOptions) {
-	const url: string = getWebSocketURL();
+    const url: string = getWebSocketURL();
 
     const {
         lastJsonMessage,
@@ -25,27 +23,27 @@ export function useAppWebSocket(options: UseAppWebSocketOptions) {
         shouldReconnect: () => true,
     });
 
-	useEffect(() => {
-		if (readyState === ReadyState.OPEN && options?.autoSyn !== false) {
-			const syn: WebsocketCommunicationC2SType = {
-				event: WebsocketEventC2SEnum.HELLO,
-				data: { status: "ok" },
-			};
-			sendJsonMessage(syn);
-		}
-	}, [readyState, sendJsonMessage, options?.autoSyn]);
-	useEffect(() => {
-		console.log(`Got a new json message: ${JSON.stringify(lastJsonMessage)}`);
-	}, [lastJsonMessage]);
+    useEffect(() => {
+        if (readyState === ReadyState.OPEN && options?.autoSyn !== false) {
+            const syn: WebsocketCommunicationC2SType = {
+                event: WebsocketEventC2SEnum.HELLO,
+                data: {status: "ok"},
+            };
+            sendJsonMessage(syn);
+        }
+    }, [readyState, sendJsonMessage, options?.autoSyn]);
+    useEffect(() => {
+        console.log(`Got a new json message: ${JSON.stringify(lastJsonMessage)}`);
+    }, [lastJsonMessage]);
 
-	return useMemo(() => {
-		return {
-			url,
-			readyState,
-			lastJsonMessage,
-			sendJsonMessage,
-			send: sendMessage,
-			isOpen: readyState === ReadyState.OPEN,
-		};
-	}, [url, readyState, lastJsonMessage, sendJsonMessage, sendMessage]);
+    return useMemo(() => {
+        return {
+            url,
+            readyState,
+            lastJsonMessage,
+            sendJsonMessage,
+            send: sendMessage,
+            isOpen: readyState === ReadyState.OPEN,
+        };
+    }, [url, readyState, lastJsonMessage, sendJsonMessage, sendMessage]);
 }
