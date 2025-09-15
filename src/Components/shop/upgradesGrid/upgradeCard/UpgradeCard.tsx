5edb489403cd3c5d995c2594e4333ae18eca9d5c
kilianrdgs <krdgs.rodrigues@gmail.com>
2025-09-15 13:26:54 +0200

    fix

--- src/Components/shop/upgradesGrid/upgradeCard/UpgradeCard.tsx
+++ src/Components/shop/upgradesGrid/upgradeCard/UpgradeCard.tsx
@@ -1,20 +1,20 @@
-import "./UpgradeCard.css";
+import "./upgradeCard.css";
 
 type UpgradeCardProps = {
-	title: string;
-	unit: string;
-	actual: string;
+  title: string;
+  unit: string;
+  actual: string;
 };
 
 export default function UpgradeCard({ title, unit, actual }: UpgradeCardProps) {
-	return (
-		<div className="upgrade-grid-card">
-			<div>
-				<p className="title">{title}</p>
-				<p className="unit">{unit}</p>
-				<p className="actual">{actual}</p>
-			</div>
-			<button className="upgrade-grid-button">Améliorer</button>
-		</div>
-	);
+  return (
+    <div className="upgrade-grid-card">
+      <div>
+        <p className="title">{title}</p>
+        <p className="unit">{unit}</p>
+        <p className="actual">{actual}</p>
+      </div>
+      <button className="upgrade-grid-button">Améliorer</button>
+    </div>
+  );
 }
